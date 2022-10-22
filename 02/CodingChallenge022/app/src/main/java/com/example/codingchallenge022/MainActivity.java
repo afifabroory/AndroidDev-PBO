a4a57af0b9086642c32362b1305b45bd857437d4
afifabroory <afif.abroory@gmail.com>
2022-10-22 18:13:57 +0800

    Set button onClick handler

--- 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
+++ 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.codingchallenge022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.codingchallenge022.databinding.ActivityMainBinding;
@@ -13,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(mainBinding.getRoot());
+
+        mainBinding.addItem.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ShoppingListActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/ShoppingListActivity.java
+++ 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/ShoppingListActivity.java
@@ -2,16 +2,39 @@ package com.example.codingchallenge022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.codingchallenge022.databinding.ActivityShoppingListBinding;
 
 public class ShoppingListActivity extends AppCompatActivity {
     private ActivityShoppingListBinding shoppingListBinding;
+    private final Button[] mButton = {
+            shoppingListBinding.button,
+            shoppingListBinding.button2,
+            shoppingListBinding.button3,
+            shoppingListBinding.button4,
+            shoppingListBinding.button5,
+            shoppingListBinding.button6,
+    };
+    private final static String ITEM_TAG = "com.example.codingchallenge022.EXTRA.ITEM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(shoppingListBinding.getRoot());
+
+        for (Button button: mButton) {
+            button.setOnClickListener(v -> {
+                Button btn = (Button) v;
+
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra(ITEM_TAG, btn.getText().toString());
+
+                setResult(RESULT_OK, intent);
+                finish();
+            });
+        }
     }
 }
