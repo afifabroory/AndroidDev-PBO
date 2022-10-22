93684f083a9d4f18f5b7f17aa9b2213cc3c6af89
afifabroory <afif.abroory@gmail.com>
2022-10-22 19:36:24 +0800

    Add ActivityResultLauncher in MainActivity

--- 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
+++ 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.codingchallenge022;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.codingchallenge022.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mainBinding;
+    private int mTextViewCount = 0;
+    private final TextView[] mTextView = {
+            mainBinding.textView,
+            mainBinding.textView2,
+            mainBinding.textView3,
+            mainBinding.textView4,
+            mainBinding.textView5,
+            mainBinding.textView6,
+            mainBinding.textView7,
+            mainBinding.textView8,
+            mainBinding.textView9,
+    };
+    private final ActivityResultLauncher<Intent> mAddShoppingList =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent intent = result.getData();
+
+                            String item = intent.getStringExtra(ShoppingListActivity.ITEM_TAG);
+                            mTextView[mTextViewCount].setText(item);
+                            mTextViewCount++;
+                        }
+                    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         mainBinding.addItem.setOnClickListener(v -> {
             Intent intent = new Intent(this, ShoppingListActivity.class);
-            startActivity(intent);
+            mAddShoppingList.launch(intent);
         });
     }
 }
