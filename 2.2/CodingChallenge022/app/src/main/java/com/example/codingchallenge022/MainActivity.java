bc3424fabe2d9dcec5297a62d1f346def96787d8
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-10-23 07:04:18 +0800

    Add: Task 1 & Task 2 (#2)
    * Initial project template (refactored)
    
    * Add Task 1
    
    * Add Task 2
    
    * Initial & Activity UI declaration
    
    * Update MainActivity & ShoppingListActivity to use view binding
    
    * Set button onClick handler
    
    * Change member access modifier in ShoppingListActivity class
    
    * Add ActivityResultLauncher in MainActivity
    
    * Refactor code
    
    * Update IDE
    
    * Fix: MainActivity error view binding
    
    * Fix: Change text view order and fix overlap view
    
    * Fix: ShoppingListActivity view binding error
    
    * Refactor code
    
    * Migrate folder location
    
    * Change Task 2 folder name
    
    * Separate Task folder name (#1)

---
+++ 2.2/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.codingchallenge022;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.codingchallenge022.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private int mTextViewCount = 0;
+    private ActivityMainBinding mainBinding;
+    private TextView[] mTextView;
+    private final ActivityResultLauncher<Intent> mAddShoppingList =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent intent = result.getData();
+
+                            String item = intent.getStringExtra(ShoppingListActivity.ITEM_TAG);
+                            mTextView[mTextViewCount].setText(item);
+                            mTextViewCount++;
+
+                            if (mTextViewCount == 9) {
+                                mainBinding.addItem.setEnabled(false);
+                            }
+                        }
+                    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        mTextView = new TextView[]{
+                mainBinding.textView,
+                mainBinding.textView2,
+                mainBinding.textView3,
+                mainBinding.textView4,
+                mainBinding.textView5,
+                mainBinding.textView6,
+                mainBinding.textView7,
+                mainBinding.textView8,
+                mainBinding.textView9,
+        };
+
+        mainBinding.addItem.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ShoppingListActivity.class);
+            mAddShoppingList.launch(intent);
+        });
+    }
+}
