f622e8bd70e517ab303e2e8b940e28fe87911e01
afifabroory <afif.abroory@gmail.com>
2022-10-22 21:08:59 +0800

    Fix: MainActivity error view binding

--- 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
+++ 02/CodingChallenge022/app/src/main/java/com/example/codingchallenge022/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.codingchallenge022;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -11,19 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.codingchallenge022.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding mainBinding;
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     private int mTextViewCount = 0;
-    private final TextView[] mTextView = {
-            mainBinding.textView,
-            mainBinding.textView2,
-            mainBinding.textView3,
-            mainBinding.textView4,
-            mainBinding.textView5,
-            mainBinding.textView6,
-            mainBinding.textView7,
-            mainBinding.textView8,
-            mainBinding.textView9,
-    };
+    private ActivityMainBinding mainBinding;
+    private TextView[] mTextView;
     private final ActivityResultLauncher<Intent> mAddShoppingList =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                     result -> {
@@ -33,13 +25,30 @@ public class MainActivity extends AppCompatActivity {
                             String item = intent.getStringExtra(ShoppingListActivity.ITEM_TAG);
                             mTextView[mTextViewCount].setText(item);
                             mTextViewCount++;
+
+                            if (mTextViewCount == 9) {
+                                mainBinding.addItem.setEnabled(false);
+                            }
                         }
                     });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
+
+        mTextView = new TextView[]{
+                mainBinding.textView,
+                mainBinding.textView2,
+                mainBinding.textView3,
+                mainBinding.textView4,
+                mainBinding.textView5,
+                mainBinding.textView6,
+                mainBinding.textView7,
+                mainBinding.textView8,
+                mainBinding.textView9,
+        };
 
         mainBinding.addItem.setOnClickListener(v -> {
             Intent intent = new Intent(this, ShoppingListActivity.class);
