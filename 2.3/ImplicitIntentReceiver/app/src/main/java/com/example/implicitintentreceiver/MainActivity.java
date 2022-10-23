03af811d273d0fccf6d39b7a33e5936f3e353cc3
afifabroory <afif.abroory@gmail.com>
2022-10-24 00:11:25 +0800

    Add: Get intent code functionality

--- 2.3/ImplicitIntentReceiver/app/src/main/java/com/example/implicitintentreceiver/MainActivity.java
+++ 2.3/ImplicitIntentReceiver/app/src/main/java/com/example/implicitintentreceiver/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.implicitintentreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.implicitintentreceiver.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        if (uri != null) {
+            String uriString = "URI: " + uri.toString();
+            mainBinding.textUriMessage.setText(uriString);
+        }
     }
 }
