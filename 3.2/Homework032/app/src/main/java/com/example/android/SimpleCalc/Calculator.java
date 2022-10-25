3bdc8f346332793c910ca341380f47eac1e84169
afifabroory <afif.abroory@gmail.com>
2022-10-25 16:25:49 +0800

    Add: New power operation

--- 3.2/Homework032/app/src/main/java/com/example/android/SimpleCalc/Calculator.java
+++ 3.2/Homework032/app/src/main/java/com/example/android/SimpleCalc/Calculator.java
@@ -24,7 +24,7 @@ import java.lang.IllegalArgumentException;
 public class Calculator {
 
     // Available operations
-    public enum Operator {ADD, SUB, DIV, MUL}
+    public enum Operator {ADD, SUB, DIV, MUL, POW}
 
     /**
      * Addition operation
@@ -53,5 +53,9 @@ public class Calculator {
      */
     public double mul(double firstOperand, double secondOperand) {
         return firstOperand * secondOperand;
+    }
+
+    public double pow(double firstOperand, double secondOperand) {
+        return Math.pow(firstOperand, secondOperand);
     }
 }
