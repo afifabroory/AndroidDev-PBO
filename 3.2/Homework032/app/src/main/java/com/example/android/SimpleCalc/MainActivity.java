d292e0cb083b23bf2a2808ce92904e08e636c417
afifabroory <afif.abroory@gmail.com>
2022-10-25 17:13:39 +0800

    Add: MainActivity layout declaration & button onClick handler

--- 3.2/Homework032/app/src/main/java/com/example/android/SimpleCalc/MainActivity.java
+++ 3.2/Homework032/app/src/main/java/com/example/android/SimpleCalc/MainActivity.java
@@ -89,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
         compute(Calculator.Operator.MUL);
     }
 
+    public void onPow(View view) { compute(Calculator.Operator.POW); }
+
     private void compute(Calculator.Operator operator) {
         double operandOne;
         double operandTwo;
@@ -118,6 +120,10 @@ public class MainActivity extends AppCompatActivity {
             case MUL:
                 result = String.valueOf(
                         mCalculator.mul(operandOne, operandTwo));
+                break;
+            case POW:
+                result = String.valueOf(
+                        mCalculator.pow(operandOne, operandTwo));
                 break;
             default:
                 result = getString(R.string.computationError);
